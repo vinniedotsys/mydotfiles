27c4c3b227c514d187878c3f16fa7a8daeec71b3
vinniedotsys <vincent.hassen@laposte.net>
2026-06-27 22:22:30 +0200

    opacity

--- hypr/conf/decoration.lua
+++ hypr/conf/decoration.lua
@@ -12,7 +12,7 @@ hl.config({
     decoration = {
         rounding = 10,
         active_opacity = 1.0,
-        inactive_opacity = 0.9,
+        inactive_opacity = 1.0,
         fullscreen_opacity = 1.0,
         blur = {
             enabled = true,
